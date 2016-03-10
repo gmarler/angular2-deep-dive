c1e41d153adc36997d5c9d6f3fb63697eb1b6917
matiboy <mathieu.chauvinc@gmail.com>
2016-03-11 07:01:55 +0800

    Plain and simple

--- app/boot.ts
+++ app/boot.ts
@@ -1,8 +1,7 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {AppComponent} from './app.component';
-import {LoggerService} from './blocks/logger.service';
+// This is how you import components in ES6
+// Absolute paths like the one below looks into node_modules
+import {bootstrap} from '@angular/platform-browser-dynamic';
+// Relative paths look up from the current file
+// import {ItunesAppComponent} from './app.component';
 
-bootstrap(AppComponent, [
-    LoggerService, ROUTER_PROVIDERS
-]);
+// Once we've created our component, we'll boostrap the application
