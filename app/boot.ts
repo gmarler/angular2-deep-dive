825e27ff09b35383cb169f58b715b98fa58da69a
matiboy <mathieu.chauvinc@gmail.com>
2016-03-11 07:27:57 +0800

    Extract footer, create second app

--- app/boot.ts
+++ app/boot.ts
@@ -3,6 +3,16 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 // Relative paths look up from the current file
 import {ItunesAppComponent} from './app.component';
+import {Component} from '@angular/core';
+
+// Don't create components inside this file, and use better naming for the class, this is just q&d demo
+@Component({
+  selector: 'inspiring-quote',
+  template: `<h4>Quote:</h4>
+  <p>Just remember, baby, you're the best!</p>
+  `
+}) class MatSays {}
 
 // Once we've created our component, we'll boostrap the application
 bootstrap(ItunesAppComponent, []);
+bootstrap(MatSays, []);
