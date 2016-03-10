344c2f83ced637a78ca4a01cd1fe07872222e927
matiboy <mathieu.chauvinc@gmail.com>
2016-03-11 07:01:55 +0800

    Component created, application bootstrapped

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,2 +1,9 @@
 import {Component} from '@angular/core';
 // We need to create a root component for the application here
+
+@Component({
+  selector: 'itunes-browser',
+  templateUrl: 'app/app.html'
+})
+export class ItunesAppComponent {
+}
--- app/boot.ts
+++ app/boot.ts
@@ -3,6 +3,7 @@
 // Absolute paths like the one below looks into node_modules
 import {bootstrap} from '@angular/platform-browser-dynamic';
 // Relative paths look up from the current file
-// import {ItunesAppComponent} from './app.component';
+import {ItunesAppComponent} from './app.component';
 
 // Once we've created our component, we'll boostrap the application
+bootstrap(ItunesAppComponent, []);
