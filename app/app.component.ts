67752cc87294e601f37a3051a948d121f670769c
matiboy <mathieu.chauvinc@gmail.com>
2016-03-13 18:22:54 +0800

    That's how DI used to work

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,12 @@
 import {Component, ElementRef} from '@angular/core';
 
+/* Just a reminder of the old days of Angular 1: remember Dependency Injection where you would "request" services?
+  angular.module('MyApp')
+    .controller('HomePageController', ['ApiService', 'NameService', function(ApiService, NameService) {
+    // Do something with those services
+  }])
+*/
+
 @Component({
   selector: 'itunes-browser',
   templateUrl: 'app/app.html'
