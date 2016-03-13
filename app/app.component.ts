509c472317bdff3f0bf49275449acdb9363aa6b6
matiboy <mathieu.chauvinc@gmail.com>
2016-03-13 11:24:48 +0800

    Declare properties on a class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,15 @@
 import {Component} from '@angular/core';
-// We need to create a root component for the application here
 
 @Component({
   selector: 'itunes-browser',
   templateUrl: 'app/app.html'
 })
 export class ItunesAppComponent {
+  public firstName;
+  public lastName;
+  constructor() {
+    this.firstName = 'Schalk';
+    this.lastName = 'Burger';
+  }
   fan() {}
 }
