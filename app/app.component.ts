fe036d655baf1a8545aaf6c4ea47b6b520e34fba
matiboy <mathieu.chauvinc@gmail.com>
2016-03-13 10:27:37 +0800

    Cleanup

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,4 +6,5 @@ import {Component} from '@angular/core';
   templateUrl: 'app/app.html'
 })
 export class ItunesAppComponent {
+  fan() {}
 }
--- app/boot.ts
+++ app/boot.ts
@@ -3,17 +3,23 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 // Relative paths look up from the current file
 import {ItunesAppComponent} from './app.component';
-import {Component} from '@angular/core';
 
-// Don't create components inside this file, and use better naming for the class, this is just q&d demo
-@Component({
-  selector: '.inspiring-quote',
-  template: `<h4>Quote:</h4>
-  <p>Just remember, baby, you're the best!</p>
-  `
-}) class MatSays {}
 
-// Once we've created our component, we'll boostrap the application
+/* Some references regarding Typescript
+Google Trends:
+http://www.google.com/trends/explore#cat=0-5-31&q=coffeescript%2C%20typescript%2C%20dart%2C%20elm%2C%20purescript&date=today%2012-m&cmpt=q&tz=Etc%2FGMT-8
+
+Blog post about Microsoft's TypeScript team working with the Angular team
+https://blogs.msdn.microsoft.com/typescript/2015/03/05/angular-2-built-on-typescript/
+
+Default Angular quickstart
+https://angular.io/docs/ts/latest/quickstart.html
+
+
+Javascript Angular quickstart shows a box hinting that you should probably go TS
+https://angular.io/docs/js/latest/quickstart.html
+*/
+
 // Lots to learn about promises,
 // I have published a 2 hours course about it :)
 // https://www.packtpub.com/web-development/learning-javascript-promises-practical-applications-es6-and-angularjs-video
@@ -23,8 +29,3 @@ bootstrap(ItunesAppComponent, [])
   }, function(err) {
     console.error('Ouch, something went wrong', err);
   });
-bootstrap(MatSays, []).then(function() {
-  console.log('Quotes app is ready to go');
-}, function(err) {
-  console.error('That s not inspiring...', err);
-});
