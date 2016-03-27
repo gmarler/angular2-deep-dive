42dc8426d7b54aff9d17f389846a70387e361cdf
matiboy <mathieu.chauvinc@gmail.com>
2016-03-28 05:36:48 +0800

    Prepare data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,14 @@
 import {Component, ElementRef} from '@angular/core';
 
-interface Person {
-  firstName: string;
-  lastName: string;
-  age?: number;
-}
+import {Track, API_RESULTS} from './data';
 
 @Component({
   selector: 'itunes-browser',
   templateUrl: 'app/app.html'
 })
 export class ItunesAppComponent {
-  public person: Person;
+  public tracks: Track[];
   constructor() {
-    this.person = <Person>{
-      firstName: 'Poe',
-      lastName: 'Dameron',
-      age: 24,
-      side: 'light'
-    };
-  }
-
-  fan() {
-    return `BB8 belongs to ${this.person.firstName} ${this.person.lastName}`;
+    this.tracks = API_RESULTS.results;
   }
 }
