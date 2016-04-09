905512e5048ce8383e3385fde2ebe746ea3c9283
matiboy <mathieu.chauvinc@gmail.com>
2016-04-10 07:50:41 +0800

    Track component setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,12 @@ import {Component, ElementRef} from '@angular/core';
 
 import {Track, API_RESULTS} from './data';
 import {SearchBarComponent} from './search/searchbar.component';
+import {TrackComponent} from './tracks/track.component';
 
 @Component({
   selector: 'itunes-browser',
   templateUrl: 'app/app.html',
-  directives: [SearchBarComponent]
+  directives: [SearchBarComponent, TrackComponent]
 })
 export class ItunesAppComponent {
   public tracks:Track[];
