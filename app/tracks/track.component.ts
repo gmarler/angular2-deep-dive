70d14fd7465ffbe007ffd01d8643808d63e8a989
matiboy <mathieu.chauvinc@gmail.com>
2016-04-10 08:10:43 +0800

    Component without input

--- app/tracks/track.component.ts
+++ app/tracks/track.component.ts
@@ -3,6 +3,15 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'track-row',
   template: `
+  <div class="row">
+    <div class="col-xs-3">
+      <img [src]="track.artworkUrl100">
+    </div>
+    <div class="col-xs-8">
+      <p class="track" [innerText]="track.trackName"></p>
+      <p class="album" [innerHtml]="'<i>'+track.collectionName+'</i>'"></p>
+    </div>
+  </div>
   `
 })
 export class TrackComponent {
