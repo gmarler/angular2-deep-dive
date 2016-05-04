fde1cb9cbe23f26c8d9f3e125eaa02d6363f138c
matiboy <mathieu.chauvinc@gmail.com>
2016-05-05 06:32:11 +0800

    Prepare to move to RC

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,17 +9,14 @@ import {TrackComponent} from './tracks/track.component';
   directives: [SearchBarComponent, TrackComponent]
 })
 export class ItunesAppComponent {
-  public tracks:Track[] = API_RESULTS.results;
+  public tracks:Track[] = [];
   public searchTerm = '';
   public typedTerm = '';
 
   constructor() {
-    API_RESULTS.results.forEach((i) => i.sources = [i.previewUrl]);
-    setInterval(() => API_RESULTS.results[0].sources.push('random stuff'),2000);
   }
 
   runTheSearch(term:string) {
-    this.searchTerm = term;
-    this.tracks = API_RESULTS.results.filter((item) => item.trackName.includes(term));
+    
   }
 }
