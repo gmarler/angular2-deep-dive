327be339ce753971b591bc0d9b81f24d0ae3c06f
matiboy <mathieu.chauvinc@gmail.com>
2016-05-07 16:44:28 +0800

    Handle error

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import {Track} from './tracks/track.model';
 import {SearchBarComponent} from './search/searchbar.component';
 import {TrackComponent} from './tracks/track.component';
@@ -19,7 +19,12 @@ export class ItunesAppComponent {
 
   runTheSearch(term:string) {
     // API url
-    this.http.get(`http://redapesolutions.com/itunes?entity=musicTrack&term=${term}`)
-      .subscribe((response) => this.tracks = response.json().results);
+    this.http.get(`http://redapesolutions.com/notfound?entity=musicTrack&term=${term}`)
+      .subscribe(
+        (response) => this.tracks = response.json().results,
+        (response:Response) => {
+          console.log(response.type, response.url);
+        }
+      );
   }
 }
