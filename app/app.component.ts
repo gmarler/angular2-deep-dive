eec1cfc20888e53e4a417111ba62ada6c0da682e
matiboy <mathieu.chauvinc@gmail.com>
2016-05-07 18:49:13 +0800

    Create our own injectable

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,25 @@
 import {Component, ElementRef} from '@angular/core';
-import {} from '@angular/http';
-import {SearchService} from './search/search.service';
 import {Track} from './tracks/track.model';
 import {SearchBarComponent} from './search/searchbar.component';
+import {SearchService} from './search/search.service';
 import {TrackComponent} from './tracks/track.component';
+import {JSONP_PROVIDERS} from '@angular/http';
 
 @Component({
   selector: 'itunes-browser',
   templateUrl: 'app/app.html',
   directives: [SearchBarComponent, TrackComponent],
-  providers: []
+  providers: [JSONP_PROVIDERS, SearchService]
 })
 export class ItunesAppComponent {
   public tracks:Track[] = [];
   public searchTerm = '';
   public typedTerm = '';
 
-  constructor() {
+  constructor(private searchService:SearchService) {
   }
 
   runTheSearch(term:string) {
+    this.searchService.search(term).subscribe((items) => this.tracks = items);
   }
 }
--- app/search/search.service.ts
+++ app/search/search.service.ts
@@ -1,15 +1,22 @@
-import {} from '@angular/core';
-import {} from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Jsonp, JSONP_PROVIDERS, URLSearchParams, RequestOptions} from '@angular/http';
 import {Track} from '../tracks/track.model';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
 
-
+@Injectable()
 export class SearchService {
-  constructor() {}
-
+  constructor(private jsonp:Jsonp) {
+  }
   search(term:string) {
     // API url
     // https://itunes.apple.com/search
+    let params = new URLSearchParams();
+    params.set('term', term);
+    params.set('callback', 'JSONP_CALLBACK');
+    return this.jsonp.get('https://itunes.apple.com/search', {
+      search: params
+    })
+      .map((response) => response.json().results);
   }
 }
