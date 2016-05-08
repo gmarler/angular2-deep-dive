d88aeffa1c0444ef0a67ddef497ece7523eafbf2
matiboy <mathieu.chauvinc@gmail.com>
2016-05-08 14:08:25 +0800

    Use multi:true to extend providers

--- app/boot.ts
+++ app/boot.ts
@@ -1,10 +1,12 @@
 // This is how you import components in ES6
 // Absolute paths like the one below looks into node_modules
 import {bootstrap} from '@angular/platform-browser-dynamic';
+import {PLATFORM_DIRECTIVES, provide} from '@angular/core';
 // Relative paths look up from the current file
 import {ItunesAppComponent} from './app.component';
+import {AudioDirective} from './tracks/audio.directive';
 
-bootstrap(ItunesAppComponent)
+bootstrap(ItunesAppComponent, [provide(PLATFORM_DIRECTIVES, {useValue: AudioDirective, multi: true})])
   .then(function() {
     console.log('Itunes Browser is ready to go');
   }, function(err) {
