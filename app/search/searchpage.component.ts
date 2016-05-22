362c81fa9a44769a6b347e666183fa5c090ea5c6
matiboy <mathieu.chauvinc@gmail.com>
2016-05-23 04:33:47 +0800

    Pipe arguments using the spread operator

--- app/search/searchpage.component.ts
+++ app/search/searchpage.component.ts
@@ -1,13 +1,25 @@
-import {Component, Input, ViewChild, AfterViewInit, OnChanges} from '@angular/core';
+import {Component, Input, ViewChild, AfterViewInit, OnChanges, Pipe, PipeTransform} from '@angular/core';
 import {SearchBarComponent} from './searchbar.component';
 import {HistoryService} from './history.service';
 import {TrackListComponent} from '../tracks/track-list.component';
 import {Router, RouteSegment, OnActivate, CanDeactivate, RouteTree} from '@angular/router';
 
+@Pipe({name:'add'})
+export class AddPipe {
+  transform(initial:number, ...args:number[]):number {
+    args.forEach(n => {
+      initial += n;
+    });
+    return initial;
+  }
+}
+
 @Component({
   template: `
+    {{4 | add:5:10:20:3}}
     <search-bar [(term)]="typedTerm" class="form-group" (execute-search)="runTheSearch($event)"></search-bar>
     <track-list></track-list>`,
+  pipes: [AddPipe],
   directives: [SearchBarComponent, TrackListComponent]
 })
 export class SearchPageComponent implements OnActivate {
