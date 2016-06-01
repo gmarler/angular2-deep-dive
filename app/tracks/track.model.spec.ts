311401e67d173e235c04fd8fa6838edcbb7c1aec
matiboy <mathieu.chauvinc@gmail.com>
2016-06-01 08:23:53 +0800

    Simple test of a class

--- app/tracks/track.model.spec.ts
+++ app/tracks/track.model.spec.ts
@@ -7,10 +7,18 @@ import {Track} from './track.model';
 
 describe('Track model Tests', () => {
   it('Creates a track object from a POJO', () => {
-    expect(false).toBe(true);
+    let track = Track.fromJson({
+      a: 41
+    });
+    expect(track instanceof Track).toBeTruthy();
   });
 
   it('Should copy all properties over', () => {
-    expect(false).toBe(true);
+    let track = Track.fromJson({
+      trackName: 'Hello',
+      artistName: 'Hi'
+    });
+    expect(track.trackName).toEqual('Hello');
+    expect(track.artistName).toEqual('Hi');
   });
 });
