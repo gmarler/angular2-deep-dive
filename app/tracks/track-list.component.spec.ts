82667e39854f54c31ffdfba83e9396ec68a0aa67
matiboy <mathieu.chauvinc@gmail.com>
2016-06-01 08:26:11 +0800

    Testing a component

--- app/tracks/track-list.component.spec.ts
+++ app/tracks/track-list.component.spec.ts
@@ -13,7 +13,32 @@ import {TestComponentBuilder, ComponentFixture} from '@angular/compiler/testing'
 import {SearchService} from '../search/search.service';
 
 describe('Track rows', () => {
-  it('Creates a track row per track', () => {
-    expect(false).toBe(true);
-  });
+  it('Creates a track row per track', async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    tcb.overrideProviders(TrackListComponent, [provide(SearchService, {
+      useValue: {
+        search: (term) => {
+          return Promise.resolve([
+            Track.fromJson({
+              trackName: 'Hi'
+            }),
+            Track.fromJson({
+              trackName: 'Hi there'
+            })
+          ]);
+        }
+      }
+    })])
+    .createAsync(TrackListComponent)
+    .then((componentFixture:ComponentFixture<TrackListComponent>) => {
+      componentFixture.detectChanges();
+      let component = componentFixture.componentRef.instance;
+      component.trackComponents.changes.subscribe((rows) => {
+        expect(rows.length).toBe(2);
+      });
+      component.search('hi').then(() => {
+        componentFixture.detectChanges();
+      });
+
+    });
+  })));
 });
