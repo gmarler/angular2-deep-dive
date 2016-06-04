55eb080cd5c6e7b24485296f3183ab1d6278fb68
matiboy <mathieu.chauvinc@gmail.com>
2016-06-04 10:58:24 +0800

    Using cache: make a rejected promise into a success

--- app/tracks/trackpage/track.service.ts
+++ app/tracks/trackpage/track.service.ts
@@ -10,27 +10,37 @@ export class TrackHolder {
   constructor(private searchService:SearchService, private cache:InMemoryCache) {}
   get track():Promise<Track> {
     if(!this._track) {
-      // Implement this in 2 different ways:
-      // 1. load track then load artist then load albums
-      // 2. load track then simultaneously load artist and albums
-      return this.searchService.getSong(this.id)
-        // Load track
+      // Try to use cache
+      return this.cache.get(this.id)
+        // Found in cache, just set
         .then(track => this._track = track)
-        // Then get both artist and albums at the same time
-        .then(track => {
-          // Issues with Promise.all typing are known https://github.com/Microsoft/TypeScript/issues/5935
-          return Promise.all<Artist | Album[]>([
-            this.searchService.getArtist(track.trackId),
-            this.searchService.loadAlbums(track.artistId)
-          ]);
-        })
-        // Then set artist on track and albums on artist
-        // Until Promise.all typing is fixed, we have to explicitely assert artist and album array
-        .then(([artist, albums]: [Artist, Album[]]) => {
-          this._track.artist = artist;
-          artist.albums = albums;
-        })
-        .then(_ => this._track);
+        // Not found, promise is rejected
+        .catch(key => {
+          console.log(`Key not found in cache ${key}`);
+          // But we can make a rejected promise into a success by returning a resolved promise
+          return this.searchService.getSong(this.id)
+            // Load track
+            .then(track => {
+              this._track = track;
+              this.cache.set(key, track);
+              return track;
+            })
+            // Then get both artist and albums at the same time
+            .then(track => {
+              // Issues with Promise.all typing are known https://github.com/Microsoft/TypeScript/issues/5935
+              return Promise.all<Artist | Album[]>([
+                this.searchService.getArtist(track.trackId),
+                this.searchService.loadAlbums(track.artistId)
+              ]);
+            })
+            // Then set artist on track and albums on artist
+            // Until Promise.all typing is fixed, we have to explicitely assert artist and album array
+            .then(([artist, albums]: [Artist, Album[]]) => {
+              this._track.artist = artist;
+              artist.albums = albums;
+            })
+            .then(_ => this._track);
+        });
     }
     return Promise.resolve(this._track);
   }
